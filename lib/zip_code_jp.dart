54d46bab64b5684833d847476333a2b647bad06c
yamarkz <y.kazuki0614n@gmail.com>
2019-10-30 23:08:35 +0900

    add function interface

--- lib/zip_code_jp.dart
+++ lib/zip_code_jp.dart
@@ -1,3 +1,7 @@
 library zip_code_jp;
 
-class ZipCodeJp {}
+class ZipCodeJp {
+  static Future<List<Map<String, dynamic>>> locate(zipCode, {opt}) async {}
+  static Map<String, dynamic> basicAddressFrom(addressParam) {}
+  static Map<String, dynamic> extendedAddressFrom(addressParam, {opt}) {}
+}
